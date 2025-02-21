4be8c7b9ae344a9928fd118d2acbf2ed2b3dbbab
Wilfredo Velazquez-Rodriguez <will.velazquez@flexibits.com>
2025-02-21 13:57:01 -0500

    Revert "Merge pull request #44 from flexibits/ste/nsurlsession-loop-break"
    This reverts commit 6ee799700dcb55aa2cc30b1de98c7aa7694d6449, reversing
    changes made to 73518d3829eac6ecb553dee3132a7b56101d065f.

--- Source/GSMultiHandle.m
+++ Source/GSMultiHandle.m
@@ -232,19 +232,17 @@ static int curl_timer_function(CURLM *multi, long timeout_ms, void *clientp)
 }
 
 - (void) readAndWriteAvailableDataOnSocket: (curl_socket_t)socket
-{
-  int numfds = 0;
-  
+{  
   do
     {
       handleMultiCode(curl_multi_perform(_rawHandle, &_runningHandlesCount));
 
       if (_runningHandlesCount)
         {
-          handleMultiCode(curl_multi_poll(_rawHandle, NULL, 0, 1000, &numfds));
+          handleMultiCode(curl_multi_poll(_rawHandle, NULL, 0, 10000, NULL));
         }
     }
-    while (_runningHandlesCount && numfds);
+    while (_runningHandlesCount);
 
   handleMultiCode(curl_multi_socket_action(_rawHandle, socket, 0, &_runningHandlesCount));
   
