b01581d2a0c2a9c98b10bb4dce7f3c78fd407601
Wilfredo Velázquez-Rodríguez <will.velazquez@flexibits.com>
2026-09-15 17:20:05 -0400

    Fix building with newer
    In C23, an empty parameter list () now means no parameters (equivalent to void) rather than the old K&R meaning of unspecified parameters. Clang 16 added -Wincompatible-function-pointer-types as a warning for this; clang 17+ promotes it to an error by default.

--- Source/Additions/NSMutableString+GNUstepBase.m
+++ Source/Additions/NSMutableString+GNUstepBase.m
@@ -330,7 +330,7 @@
       unichar	(*caiImp)(NSString*, SEL, NSUInteger);
       SEL caiSel = @selector(characterAtIndex:);
 
-      caiImp = (unichar (*)())[self methodForSelector: caiSel];
+      caiImp = (unichar (*)(NSString*, SEL, NSUInteger))[self methodForSelector: caiSel];
       while (start < length && space((*caiImp)(self, caiSel, start)))
 	{
 	  start++;
@@ -355,7 +355,7 @@
       unichar	(*caiImp)(NSString*, SEL, NSUInteger);
       SEL caiSel = @selector(characterAtIndex:);
 
-      caiImp = (unichar (*)())[self methodForSelector: caiSel];
+      caiImp = (unichar (*)(NSString*, SEL, NSUInteger))[self methodForSelector: caiSel];
       while (end > 0 && space((*caiImp)(self, caiSel, end - 1)))
 	{
 	  end--;
